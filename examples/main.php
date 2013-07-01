d6707d5605080121594d4c25b176840db21127fc
Matthew Harris <mattbenharris@gmail.com>
2013-07-01 15:48:35 -0700

    MBH: changed to hyde file strct

---
+++ examples/main.php
@@ -0,0 +1,7 @@
+<?php
+  include_once "../layout/header.html";
+  include_once 'example.md';
+  include_once '../layout/footer.html';
+?>
+
+
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include_once 'header.html';
-  include_once 'index.md';
-  include_once 'footer.html';
-?>
-
-
